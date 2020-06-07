f9cf22a984c54d83f6bb01dcfe38aedaf680a8a9
Jeremie Tamburini <jeremie.tamburini@gmail.com>
2020-06-07 20:50:54 +0200

    Exercise end

--- Introduction_to_Programming_with_Ruby/02_intro_to_programming/09_Exercise/16.rb
+++ Introduction_to_Programming_with_Ruby/02_intro_to_programming/09_Exercise/16.rb
@@ -9,13 +9,75 @@
 # 
 # contact_data = ["joe@email.com", "123 Main st.", "555-123-4567"]
 # contacts = {"Joe Smith" => {}}
+
+
+# FIRST PART
+#
+# contact_data = ["joe@email.com", "123 Main st.", "555-123-4567"]
 # 
+# contacts = {"Joe Smith" => {}}
+# # This is how the hash will be built:
+# # contacts = {"Joe Smith" => {hash[item] => contact_data.shift, ... }}
+# 
+# items = [:email, :address, :phone]
+# 
+# contacts.each do |name, hash|
+#   items.each { |item| hash[item] = contact_data.shift }
+# end
+
+
+# SECOND PART
+#
 # As a bonus, see if you can figure out how to make it work with multiple entries
 # in the contacts hash.
+# 
+# Expected output:
+#  {
+#    "Joe Smith"=>{:email=>"joe@email.com", :address=>"123 Main st.", :phone=>"555-123-4567"},
+#    "Sally Johnson"=>{:email=>"sally@email.com", :address=>"404 Not Found Dr.",  :phone=>"123-234-3454"}
+#  }
 
 
-contact_data = ["joe@email.com", "123 Main st.", "555-123-4567"]
-contacts = {"Joe Smith" => {}}
+# items = [:email, :address, :phone]
+# 
+# contact_data = [
+#   ["joe@email.com", "123 Main st.", "555-123-4567"],
+#   ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]
+# ]
+# 
+# contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
+# 
+# contacts.each do |name, hash|
+#   array = contact_data.shift 
+#   items.each { |item| hash[item] = array.shift }
+# end
+# 
+# Thoughts:
+# I have noticed that both contacts and contact_data require 2 iterations.
+# So, before items.each (3 iterations), I have set the variable array = contact_data.shift.
+# This way I get at the first iteration:
+# "Joe Smith" => {} ["joe@email.com"  and  "123 Main st.", "555-123-4567"]
+# items.each will fill the empty array.
 
 
+#  SOLUTION
 
+contact_data = [
+  ["joe@email.com", "123 Main st.", "555-123-4567"],
+  ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]
+]
+contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
+fields = [:email, :address, :phone]
+
+contacts.each_with_index do |(name, hash), idx|
+  fields.each { |field| hash[field] = contact_data[idx].shift }
+end
+
+# Thoughts:
+# Folllowing the previous "thoughts", contacts and contact_data have got
+# the same iteration step.
+# In this example #each_with_index has been used, which is good because
+# during the iteration the idx of contacts will be exactly the same
+# of the idx of contact_data.... which can be easily iterated.
+
+p contacts
