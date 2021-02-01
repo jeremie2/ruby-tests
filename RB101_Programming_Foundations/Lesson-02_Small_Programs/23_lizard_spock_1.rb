082d270f7bea54160afe9ea1437e836169003469
Jeremie Tamburini <jeremie.tamburini@gmail.com>
2021-02-01 15:21:43 +0100

    lizard spock 1

---
+++ RB101_Programming_Foundations/Lesson-02_Small_Programs/23_lizard_spock_1.rb
@@ -0,0 +1,47 @@
+VALID_CHOICES = %w(rock paper scissors spock lizard)
+
+WIN_COMBINATIONS = [%w(paper rock), %w(paper spock), %w(rock scissors),
+                    %w(rock lizard), %w(scissors paper), %w(scissors lizard),
+                    %w(lizard spock), %w(lizard paper), %w(spock rock),
+                    %w(spock scissors)]
+
+def prompt(message)
+  Kernel.puts("=> #{message}")
+end
+
+def win?(first, second)
+  WIN_COMBINATIONS.include?([first, second])
+end
+
+def result(player, computer)
+  if win?(player, computer)
+    'You won!'
+  elsif win?(computer, player)
+    'Computer won!'
+  else
+    "It's a tie."
+  end
+end
+
+loop do
+  choice = ''
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    choice = Kernel.gets().chomp()
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("That's not a valid choice.")
+    end
+  end
+
+  computer_choice = VALID_CHOICES.sample
+  prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
+  prompt(result(choice, computer_choice))
+
+  prompt('Do you want to play again?')
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
+end
+
+prompt('Thank you for playing. Goodbye!')
