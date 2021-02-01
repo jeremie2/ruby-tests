f88e03753ef02250f264903eb378b66d71a47085
Jeremie Tamburini <jeremie.tamburini@gmail.com>
2021-02-01 17:45:48 +0100

    lizard spock 2

--- RB101_Programming_Foundations/Lesson-02_Small_Programs/23_lizard_spock_2.rb
+++ RB101_Programming_Foundations/Lesson-02_Small_Programs/23_lizard_spock_2.rb
@@ -1,23 +1,29 @@
-# The goal of this bonus is to add Lizard and Spock into your code
-# following the rules:
-#
-#  - Scissors cuts Paper
-#  - Paper covers Rock
-#  - Rock crushes Lizard
-#  - Lizard poisons Spock
-#  - Spock smashes Scissors
-#  - Scissors decapitates Lizard
-#  - Lizard eats Paper
-#  - Paper disproves Spock
-#  - Spock vaporizes Rock
-#  - Rock crushes Scissors
+# Typing the full word "rock" or "lizard" is tiring. Update the program
+# so the user can type "r" for "rock," "p" for "paper," etc.
+# Note that if you do bonus #1, you'll have two words that start with "s."
+# How do you resolve that?
 
-VALID_CHOICES = %w(rock paper scissors spock lizard)
+VALID_CHOICES = %w(r p sc sp l)
+
+SELECT_ITEM = { 'r' => 'rock',
+                'p' => 'paper',
+                'sc' => 'scissors',
+                'sp' => 'spock',
+                'l' => 'lizard' }
 
 WIN_COMBINATIONS = [%w(paper rock), %w(paper spock), %w(rock scissors),
                     %w(rock lizard), %w(scissors paper), %w(scissors lizard),
                     %w(lizard spock), %w(lizard paper), %w(spock rock),
                     %w(spock scissors)]
+
+CHOOSE = <<-MSG
+  Choose one:
+  r) rock
+  p) paper
+  sc) scissors
+  sp) spock
+  l) lizard
+MSG
 
 def prompt(message)
   Kernel.puts("=> #{message}")
@@ -40,16 +46,17 @@ end
 loop do
   choice = ''
   loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    choice = Kernel.gets().chomp()
+    prompt(CHOOSE)
+    choice = Kernel.gets().chomp().downcase()
     if VALID_CHOICES.include?(choice)
+      choice = SELECT_ITEM[choice]
       break
     else
       prompt("That's not a valid choice.")
     end
   end
 
-  computer_choice = VALID_CHOICES.sample
+  computer_choice = SELECT_ITEM[VALID_CHOICES.sample]
   prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
   prompt(result(choice, computer_choice))
 
