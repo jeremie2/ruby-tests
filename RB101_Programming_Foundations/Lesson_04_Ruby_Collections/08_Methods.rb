e974cb1eaff940c6c13a22481fd9a9aa2813f9e6
Jeremie Tamburini <jeremie.tamburini@gmail.com>
2021-03-22 02:22:55 +0100

    selection and transformation

---
+++ RB101_Programming_Foundations/Lesson_04_Ruby_Collections/08_Methods.rb
@@ -0,0 +1,120 @@
+############# SELECTION AND TRANSFORMATION
+
+# Looping can be very repetitive. Ruby's methods can make work shorter.
+
+###### EACH
+
+# We can usually loop through a collection like this:
+
+numbers = [1, 2, 3]
+counter = 0
+
+loop do
+  break if counter == numbers.size
+  puts numbers[counter]
+  counter += 1
+end
+
+# using #each method things cn be written like this 
+
+[1, 2, 3].each do |num|
+  puts num
+end
+
+# let's break the code:
+
+[1, 2, 3]                   # The array
+
+.each                       # The method called on the array
+
+               do |num|     # The block taken from the methods.
+  puts num                  # This code will be executed for each iteration
+end                         # Num is the value of the current element sent by #each
+
+
+# #each can be called on hashes. In that case it needs two arguments:
+
+hash = { a: 1, b: 2, c: 3 }
+
+hash.each do |key, value|
+  puts "The key is #{key} and the value is #{value}"
+end
+
+# An important aspect is that #each will always return the original collection.
+# This because 'hash.each' is the last expression.
+
+
+###### SELECT
+
+# Instead performing a loop setting break point and counter, it's
+# possible to select specific elements using #select method:
+
+[1, 2, 3].select do |num|
+  # num.odd?
+end
+
+# #select evaluates the return value of the block for each iteration.
+# If the return value is 'thruty' then the current element will be
+# appended to a new array.
+# Basically #select returns a new array made of elements that return
+# 'true' to a condition.
+#
+# Just to understand, let's see some 'silly' example:
+
+[1, 2, 3].select do |num|
+  num + 1
+end
+
+# 'num + 1' is a thruty condition, then every single element will be
+# added to the return array [1, 2, 3].
+
+[1, 2, 3].select do |num|
+  num + 1
+  puts num
+end
+
+# The last evaluated expression is not thruty as 'puts' returns 'nil'.
+# In this case no elements will be added to the new array [].
+# 
+# !! THIS CAN BE AN ERROR WHILE DEBUGGING
+# (It happens to forget a 'put' statement inside an iteration block)
+
+
+###### MAP
+
+# #map uses the return value to perform transformations and return a new array:
+
+[1, 2, 3].map do |num|
+  num * 2
+end
+
+# => [2, 4, 6]
+
+
+[1, 2, 3].map do |num|
+  num.odd?
+end
+
+# => [true, false, true]
+
+
+####### A USEFUL TABLE
+
+##############################################################
+#        #            # Considers #             #            #
+#        #            # return    # Returns     # Returned   #
+# Method #  Action    # block's   # new         # collection #
+#        #            # value?    # collection? # length     #
+##############################################################
+#        ::           ::          ::   No       ::           #
+# each   :: Iteration ::   No     :: (returns   :: Original  #
+#        ::           ::          ::  original) ::  length   #
+#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::# 
+#        ::           ::          ::            :: Original  #
+# select :: Selection ::   Yes    ::    Yes     ::  length   #
+#        ::           :: (truthy) ::            ::  or less  #
+#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::#
+#        :: Transfor- ::          ::            ::           #
+# map    ::  mation   ::   Yes    ::    Yes     :: Original  #
+#        ::           ::          ::            ::  length   #
+##############################################################
