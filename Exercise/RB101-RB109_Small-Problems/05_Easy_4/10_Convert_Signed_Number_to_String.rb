856997d75c82b9f058528b0d9f3a8cb4a9b1a8e1
Jeremie Tamburini <jeremie.tamburini@gmail.com>
2021-04-10 02:40:10 +0200

    typo

--- Exercise/RB101-RB109_Small-Problems/05_Easy_4/10_Convert_Signed_Number_to_String.rb
+++ Exercise/RB101-RB109_Small-Problems/05_Easy_4/10_Convert_Signed_Number_to_String.rb
@@ -15,6 +15,8 @@
 # signed_integer_to_string(0) == '0'
 
 
+#### ANSWER
+
 DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 def integer_to_string(number)
@@ -39,18 +41,11 @@ end
 
 #### SOLUTION
 
-# def signed_integer_to_string(number)
-#   case number <=> 0
-#   # number <=> 0  +1 if number > 0, -1 if number < 0, 0 if number == 0
-#   when -1 then "-#{integer_to_string(-number)}"
-#   when +1 then "+#{integer_to_string(number)}"
-#   else         integer_to_string(number)
-#   end
-# end
-
-
-
-puts signed_integer_to_string(4321) == '+4321'
-puts signed_integer_to_string(-123) == '-123'
-puts signed_integer_to_string(0) == '0'
-
+def signed_integer_to_string(number)
+  case number <=> 0
+  # number <=> 0  +1 if number > 0, -1 if number < 0, 0 if number == 0
+  when -1 then "-#{integer_to_string(-number)}"
+  when +1 then "+#{integer_to_string(number)}"
+  else         integer_to_string(number)
+  end
+end
