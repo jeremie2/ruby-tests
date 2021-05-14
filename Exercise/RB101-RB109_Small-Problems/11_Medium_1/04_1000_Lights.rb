f82a8b36306a379bfc1f385cca5aa434635a4796
Jeremie Tamburini <jeremie.tamburini@gmail.com>
2021-05-14 02:54:10 +0200

    Medium 1

--- Exercise/RB101-RB109_Small-Problems/11_Medium_1/04_1000_Lights.rb
+++ Exercise/RB101-RB109_Small-Problems/11_Medium_1/04_1000_Lights.rb
@@ -48,21 +48,92 @@
 #  1%5 2%5 3%5 4%5 5%5
 #                   x
 
+def switch_light(val)
+  val == 'on' ? 'off' : 'on'
+end
+
+def lights_on(num_ligths)
+  # create hash with n "switches" as keys a 'off' as status
+  hsh = Hash.new
+  1.upto(num_ligths) { |i| hsh[i] = 'off' }
+  
+  # switching on/off the lights whether key % i == 0
+  1.upto(num_ligths) do |i|
+    hsh.each do |key, val|
+      hsh[key] = switch_light(val) if key % i == 0
+    end
+  end
+  # returning the array of lights on based on key values
+  hsh.keys.select { |key| hsh[key] == 'on' }
+end
+
+p lights_on(5)    # [1, 4]
+p lights_on(10)   # [1, 4, 9]
+p lights_on(16)   # [1, 4, 9, 16]
+p lights_on(25)   # [1, 4, 9, 16, 25]
 
 #### SOLUTION
 
+# initialize the lights hash
+def initialize_lights(number_of_lights)
+  lights = Hash.new
+  1.upto(number_of_lights) { |number| lights[number] = "off" }
+  lights
+end
 
+# return list of light numbers that are on
+def on_lights(lights)
+  lights.select { |_position, state| state == "on" }.keys
+end
+
+# toggle every nth light in lights hash
+def toggle_every_nth_light(lights, nth)
+  lights.each do |position, state|
+    if position % nth == 0
+      lights[position] = (state == "off") ? "on" : "off"
+    end
+  end
+end
+
+# Run entire program for number of lights
+def toggle_lights(number_of_lights)
+  lights = initialize_lights(number_of_lights)
+  1.upto(lights.size) do |iteration_number|
+    toggle_every_nth_light(lights, iteration_number)
+  end
+
+  on_lights(lights)
+end
+
+p toggle_lights(1000)
 
 
 #### FURTHER EXPLORATION
 
+# Do you notice the pattern in our answer? Every light that is on is a perfect 
+# square. Why is that?
 
+# What are some alternatives for solving this exercise? What if we used an Array 
+# to represent our 1000 lights instead of a Hash, how would that change our code?
 
-#### ANSWER
+def switch_light(val)
+  val == 'on' ? 'off' : 'on'
+end
 
+def lights_on(num_ligths)
+  arr = Array.new
+  0.upto(num_ligths - 1) { |i| arr[i] = 'off' }
+  
+  1.upto(num_ligths) do |i|
+    arr.each_with_index do |el, idx|
+      arr[idx] = switch_light(el) if (idx + 1) % i == 0
+    end
+  end
+  idx_lights = []
+  arr.each_with_index { |el, idx| idx_lights << (idx + 1) if el == 'on' }
+  idx_lights
+end
 
-
-
-
-#### from user 
-
+# We could have a method that replicates the output from the description of this 
+# problem (i.e. "lights 2, 3, and 5 are now off; 1 and 4 are on.") How would we go 
+# about writing that code?
--- Exercise/RB101-RB109_Small-Problems/11_Medium_1/05_Diamonds.rb
+++ Exercise/RB101-RB109_Small-Problems/11_Medium_1/05_Diamonds.rb
@@ -1,26 +1,102 @@
 # Write a method that displays a 4-pointed diamond in an n x n grid, where n is an 
 # odd integer that is supplied as an argument to the method. You may assume that 
 # the argument will always be an odd integer.
-
+# 
+# diamond(1)
+# 
+# *
+# 
+# diamond(3)
+# 
+#  *
+# ***
+#  *
+# 
+# diamond(9)
+# 
+#     *
+#    ***
+#   *****
+#  *******
+# *********
+#  *******
+#   *****
+#    ***
+#     *
 
 #### ANSWER
 
+# Example odd_int = 5
+# - I build a 'pattern' array [1, 2, 3, 4, 5, 4, 3, 2, 1]
+# - Each element of the array determines the horizontal raw made of '*'
+#   that will be centered during the #each loop
 
+def pattern(size)
+  pattern = []
+  1.upto(size) { |i| pattern << i if i.odd? }
+  (size - 1).downto(1) { |i| pattern << i if i.odd? }
+  pattern
+end
+
+def diamond(size)
+  pattern = pattern(size)
+
+  pattern.each do |i|
+    puts ('*' * i).center(size)
+  end
+end
+
+diamond(9)
 
 #### SOLUTION
 
+def print_row(grid_size, distance_from_center)
+  number_of_stars = grid_size - 2 * distance_from_center
+  stars = '*' * number_of_stars
+  puts stars.center(grid_size)
+end
 
+def diamond(grid_size)
+  max_distance = (grid_size - 1) / 2
+  max_distance.downto(0) { |distance| print_row(grid_size, distance) }
+  1.upto(max_distance)   { |distance| print_row(grid_size, distance) }
+end
 
 
 #### FURTHER EXPLORATION
 
-
+# Try modifying your solution or our solution so it prints only the outline of  
+# the diamond:
+# 
+# diamond(5)
+# 
+#   *
+#  * *
+# *   *
+#  * *
+#   *
 
 #### ANSWER
 
+# Example odd_int = 5
+# - I build a 'pattern' array that matches the empty spaces inside
+#   the shape. [1, 3, 1]
+# - These empty spaces will be surrounded by two '*'
+# - To make things easier I just print the first and last line
 
+def pattern(size)
+  pattern = []
+  1.upto(size - 2) { |i| pattern << i if i.odd? }
+  (size - 4).downto(0) { |i| pattern << i if i.odd? }
+  pattern + []
+end
 
+def diamond(size)
+  pattern = pattern(size)
 
-
-#### from user 
-
+  puts ('*').center(size)
+  pattern.each do |i|
+    puts ('*' + " " * i + '*').center(size)
+  end
+  puts ('*').center(size)
+end
